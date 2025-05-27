0b28650fb280fe4778fe36879b04055691fa3394
anjanStha <anjanstha61@gmail.com>
2025-05-27 14:45:31 +1000

    update ship and background

--- game_project/main.py
+++ game_project/main.py
@@ -1,90 +1,119 @@
 import pygame
 import sys
+import os
+pygame.font.init()
 
 # initialze Pygame
 pygame.init()
 
 #DEFINE SIZE OF THE WINDOW
-window_width, window_height = 750, 750
+WIDTH, HEIGHT = 750, 750
 
 ## game screen size
-window_screen = pygame.display.set_mode((window_width, window_height))
+window_screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 # SET TITLE
 pygame.display.set_caption('My Game')
 
-# Colors
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GRAY = (169, 169, 169)
-BLUE = (0, 0, 255)
-DARK_GRAY = (50, 50, 50)
 
-# Spaceship properties
-ship_x = window_width // 10
-ship_y = window_height // 10
-ship_speed = 5
+# Load images for game
+RED_SPACE_SHIP = pygame.image.load('game_project/image/ship_enemy_red.png').convert_alpha()
+GREEN_SPACE_SHIP = pygame.image.load('game_project/image/ship_enemy_green.png').convert_alpha()
+BLUE_SPACE_SHIP = pygame.image.load('game_project/image/ship_enemy_blue.png').convert_alpha()
 
-## Create Space ship
-def draw_spaceShip(x, y):
-    # Main body
-    pygame.draw.polygon(window_screen, DARK_GRAY, [
-        (x, y - 40),  # Tip
-        (x - 25, y + 40),  # Left base
-        (x + 25, y + 40)   # Right base
-     ])
-    # Cockpit (smaller triangle)
-    pygame.draw.circle(window_screen, BLUE, 
-        (x, y  + 15), 10)
+# Player ship image
+PLAYER_SPACE_SHIP = pygame.image.load('game_project/image/player_ship.png').convert_alpha()
 
-     # Wings
-    pygame.draw.polygon(window_screen, DARK_GRAY, [
-        (x - 25, y + 10),
-        (x - 45, y + 40),
-        (x - 25, y + 40)
-        ])
-    pygame.draw.polygon(window_screen, DARK_GRAY, [
-        (x + 25, y + 10),
-        (x + 45, y + 40),
-        (x + 25, y + 40)
-        ])
-    
-    # Thruster flames
-    pygame.draw.rect(window_screen, RED, (x - 8, y + 30, 16, 15))
-    pygame.draw.rect(window_screen, RED, (x - 5, y + 45, 10, 10))
+# Lasers image
+RED_LASER = pygame.image.load('game_project/image/laser_red.png').convert_alpha()
+GREEN_LASER = pygame.image.load('game_project/image/laser_green.png').convert_alpha()
+BLUE_LASER = pygame.image.load('game_project/image/laser_blue.png').convert_alpha()
+YELLOW_LASER = pygame.image.load('game_project/image/laser_yellow.png').convert_alpha()
+
+# Background
+BG = pygame.transform.scale(pygame.image.load('game_project/image/background.png').convert_alpha(), (WIDTH, HEIGHT))
 
 
+## ship class
+class Ship:
+    def __init__(self, x, y,  health= 100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        window.blit(self.ship_img, (self.x, self.y))
+
+## Player class
+class Player(Ship):
+    def __init__(self, x, y, health=100):
+        super().__init__(x, y, health)
+        self.ship_img = PLAYER_SPACE_SHIP
+        self.laser_img = YELLOW_LASER
+        self.mask = pygame.mask.from_surface(self.ship_img)
+        self.max_health = health
 
 
-is_running = True
-surface = pygame.Surface((100, 100))
- 
-## Game running loop 
-while is_running:
-    window_screen.fill(WHITE)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            is_running = False
-    
 
-    # Movement controls
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and ship_x > 40:
-        ship_x -= ship_speed
-    if keys[pygame.K_RIGHT] and ship_x < window_width - 40:
-        ship_x += ship_speed
-    if keys[pygame.K_UP] and ship_y > 60:
-        ship_y -= ship_speed
-    if keys[pygame.K_DOWN] and ship_y < window_height - 60:
-        ship_y += ship_speed
-    
-    draw_spaceShip(ship_x, ship_y)
 
-    #window_screen.fill('gray')
-   # window_screen.blit(surface, (400, 400))
-    pygame.display.update()
-    clock.tick(60)
+# main game loop
+def main():
+    run = True
+    FPS = 60
+    level = 1
+    lives = 5
+    main_font = pygame.font.SysFont("ArialTimes New Roman", 40)
 
-pygame.quit
+    player_vel = 5
+    player = Player(300, 650)
+
+    clock = pygame.time.Clock()
+
+    #Create function within function
+    def redraw_window():
+        window_screen.blit(BG, (0,0))
+
+        #Draw text show score and life
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255))
+        level_label = main_font.render(f"level: {level}", 1, (255,255,255))
+
+        window_screen.blit(lives_label, (10, 10))
+        window_screen.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+
+        player.draw(window_screen)
+        pygame.display.update()
+
+
+
+
+    while run:
+        clock.tick(FPS)
+        redraw_window()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        ##key press
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a] and player.x - player_vel > 0: # left move
+            player.x -= player_vel
+        if keys[pygame.K_d] and player.x + player_vel + 50 < WIDTH: # right move
+            player.x += player_vel
+        if keys[pygame.K_w] and player.y - player_vel > 0: # up move
+            player.y -= player_vel
+        if keys[pygame.K_s] and player.y + player_vel + 50 < HEIGHT: # right move
+            player.y += player_vel
+
+
+
+
+### Run when this file is executed
+if __name__ == '__main__' :
+    main()
+
 
 
